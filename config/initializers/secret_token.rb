a2025e75c349b15c6056e3194b8715af68dd3347
Jun Jensen <junjensen@gmail.com>
2013-06-24 13:59:32 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChknoutRails::Application.config.secret_token = 'ee6731bb6d6bf14789bf58b63f9ddc8f9c800ed5228fa8bf5844dabda24109149236ce32237a8ada461b48278bfe3273da5baac3b457a34b9731359e04d6b570'
